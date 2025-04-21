d4d3570b1bb2937dc42c65037c4b47c64398354a
tulsi-parmar1 <tulsiparmar878@gmail.com>
2025-04-21 18:49:40 +0530

    Initial commit

---
+++ client/src/components/shop/order/FetchApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+const apiURL = "http://localhost:8000";
+export const getBrainTreeToken = async () => {
+  let uId = JSON.parse(localStorage.getItem("jwt")).user._id;
+  try {
+    let res = await axios.post(`${apiURL}/api/braintree/get-token`, {
+      uId: uId,
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getPaymentProcess = async (paymentData) => {
+  try {
+    let res = await axios.post(`${apiURL}/api/braintree/payment`, paymentData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createOrder = async (orderData) => {
+  try {
+    let res = await axios.post(`${apiURL}/api/order/create-order`, orderData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ client/src/components/shop/wishlist/FetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const apiURL = "http://localhost:8000";
+export const wishListProducts = async () => {
+  let productArray = JSON.parse(localStorage.getItem("wishList"));
+  try {
+    let res = await axios.post(`${apiURL}/api/product/wish-product`, {
+      productArray,
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
