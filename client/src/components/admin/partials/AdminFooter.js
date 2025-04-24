e73df0222f0357158f7b5186e0a69f2d807499bf
tulsi-parmar1 <tulsiparmar878@gmail.com>
2025-04-24 16:24:30 +0530

    commit

--- client/src/components/admin/partials/AdminFooter.js
+++ client/src/components/admin/partials/AdminFooter.js
@@ -8,7 +8,7 @@ const AdminFooter = (props) => {
         style={{ background: "#303031", color: "#87898A" }}
         className="z-10 py-6 px-4 md:px-12 text-center"
       >
-        Developed By BruteForce © Copyright {moment().format("YYYY")}
+        Developed By Priyank Mer © Copyright {moment().format("YYYY")}
       </footer>
     </Fragment>
   );
